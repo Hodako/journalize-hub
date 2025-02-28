a68f8086233689844b780f1bdb307cbe82ad69e3
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-28 21:52:52 +0600

    Update AuthorArticle.tsx

--- src/pages/AuthorArticle.tsx
+++ src/pages/AuthorArticle.tsx
@@ -1,4 +1,4 @@
- import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
